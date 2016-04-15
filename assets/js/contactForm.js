e97a5101a6c480b4e29465dfed30fc190a41a2ec
Linus <linusvanelswijk@gmail.com>
2016-04-15 18:52:53 +0200

    first version of contact form

---
+++ assets/js/contactForm.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Linus on 15-4-2016.
+ */
+
+$(function() {
+    var form = $('#contactForm');
+    var formMessages = $('#contactFormMessage');
+    var clear = $('#contactForm button.reset');
+
+
+    $(form).submit(function(event) {
+        event.preventDefault();
+        var formData = $(form).serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+
+            $(formMessages).text(response);
+
+            $(form).hide();
+        })
+        .fail(function(data) {
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            }
+            else {
+                $(formMessages).text('An error occured. Could not sent the message.');
+            }
+        });
+    });
+});
